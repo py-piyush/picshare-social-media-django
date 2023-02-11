b4101d4632faadde840488498fe7157602747b86
Piyush <impiyush016@gmail.com>
2023-02-11 12:18:26 +0530

    Change password view

--- socialsite/account/urls.py
+++ socialsite/account/urls.py
@@ -7,6 +7,17 @@ urlpatterns = [
     # path("login/", views.user_login, name="login")
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # change password
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     # dashboard
     path("", views.dashboard, name="dashboard"),
 ]
