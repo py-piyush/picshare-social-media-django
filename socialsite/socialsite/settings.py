e2ff3d799af6f05062f1bb0c993b419c0f5655de
Piyush <impiyush016@gmail.com>
2023-02-13 11:11:14 +0530

    User profile for social authentication

--- socialsite/socialsite/settings.py
+++ socialsite/socialsite/settings.py
@@ -148,6 +148,20 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+# Social auth pipeline
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
+
 # To handle cancellation during social authentication
 SOCIAL_AUTH_LOGIN_ERROR_URL = "login"
 # DEBUG = False
@@ -162,3 +176,4 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
     "845351220964-6u2dps3cuf3afrhrejhbqg62v2rjv10q.apps.googleusercontent.com"
 )
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ["https://www.googleapis.com/auth/userinfo.email"]
