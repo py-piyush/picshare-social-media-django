df1bdc8c2799dd19b4eb77018e2409e668c39698
Piyush <impiyush016@gmail.com>
2023-02-15 12:03:50 +0530

    Like/unlike

--- socialsite/images/urls.py
+++ socialsite/images/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("create/", views.image_bookmark, name="create"),
     path("post/", views.image_post, name="post"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
+    path("like/", views.image_like, name="like"),
 ]
